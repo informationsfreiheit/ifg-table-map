015d1d7d279a038eb4ec54fbb137b659cad1a8d1
klml <m@klml.de>
2016-07-23 21:11:20 +0200

    rm icon condition, use string to variable trick

--- js/ubersicht.js
+++ js/ubersicht.js
@@ -24,30 +24,27 @@ jQuery(document).ready(function() {
     // get city name, pos and pdf from table
     $.each( $('#woinbayern tr'), function() { 
         var pos = $(this).data('pos') ;
-        var gemart = $(this).data('gemart') ;
         var cityname = $(this).attr('id') ;
         var name = $(this).find('.name').text() ;
         var satzlink = $(this).find('.satzlink').html(); 
 
-        if ( gemart == 'Landkreis' ) {
-            var icon = blueIcon ;
-        } else if ( gemart == 'Ausarbeitung' ) {
-            var icon = yellowIcon ;
-        } else if ( gemart == 'ausserkraft' ) {
-            var icon = blackIcon ;
-        } else {
-            var icon = redIcon ;
-        }
+        var iconString = 'icon' + $(this).data('gemart') ;
 
+        // add bullets to map
+        // window[iconString] trick from http://stackoverflow.com/a/5613859/2248997
         if ( typeof pos != 'undefined' && pos != 'none' ) {
             pos = pos.split(',');
-            L.marker([ Number(pos[0]), Number(pos[1]) ], {icon: icon}).bindPopup( satzlink + "<a href='#" + cityname + "'>" + name + "</a>" ).addTo(map);
+            L.marker([ Number(pos[0]), Number(pos[1]) ], {icon: window[iconString] }).bindPopup( satzlink + "<a href='#" + cityname + "'>" + name + "</a>" ).addTo(map); 
         }
     }); 
 });
 
-// payload
-var redIcon = new LeafIcon({iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABmJLR0QA/wD/AP+gvaeTAAAAv0lEQVQYlTXPIU8CYQCA4ecDIZlMUBmb3epvOGYmOjVRbhb/g93oHyAddJKbg81sh00OdzvGeZDwzqC+9UkvvzVwNyC5JMENwp8JD0zXcVyvhsN63u/XT9Q9JghN3I7j+P5sNALt7ZaiEMryfM6qEREdNxv72cwxTakq7VZLFx0GJzn1x2KhU5ZUlf1yqTgc7FChueL0Is8HisJXlknz3DrLvOKNx4DQI4mIutjhHS9MMq7+VwKuu0Tf+GSKZ9Q/0IFOVMgrTdIAAAAASUVORK5CYII%3D'});
-var blueIcon = new LeafIcon({iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABmJLR0QA/wD/AP+gvaeTAAAA3klEQVQYlT2PsU7CQABA3yGXQBhdbN2I0Z3Vb7hGJxNHo04uxIWV+AuGEb+h7ayTDrK4aIjBSZdKzqINd6Q13DFgfOvLGx6sqQHnhFHM5n4MnALizyHY7aXd64k/vnrxO0e3ns7A02ongNgAzrr9m8uLw21AMDOCwjrmldwjf/yoESr1Oau4e/omyyucByklNAJobkV1ytyPnjPmiyXOw3tmsNbC7w84R52vh/Rt8hoZY5BSYq1Faw3FGMppIgBBqx0TKkUjWJfFGPR9QqUPxP8JnNAMFG4J5TQFhoBfAVS8XfVK8EgaAAAAAElFTkSuQmCC'});
-var yellowIcon = new LeafIcon({iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH2wcOEyUlW0X05gAAANNJREFUGJU1z7EOwVAAQNH7lE6dxNCuIrH3P9qdUWKziIHJD9iNfsDGbpI0lYjRYvISLZHSajtVnwF3PdPV+FYB+q7LtNGgIyUGcPgZYjJhHQRDJWVX+X5LzeeoZpMVIDSgv1wOR/X6AABdfwAJQqRt30dWHAenKK5k2YaiCIESXa9hWWCauNUoQl0uO0wzBUqy7EyS5MQxlCVoUmLYduRCwut1JwwjguCO58F+z0wDDscjthBp+/mMOZ1yPA+2W1Z5zlj8T4CeZeG833C7sQYWgPoAC4FeOn1WF+AAAAAASUVORK5CYII%3D'});
-var blackIcon = new LeafIcon({iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABmJLR0QA/wD/AP+gvaeTAAAAcklEQVQYlXWPSwqDQBBE35szuDQ70UPFEybiGfRAisZ9Jos0cQhYUFBNV/UHvripg/oKjkBL0VzU/McVqIlkBnp1UifgHqYn6hHFDFRAFcas7okT70J7Kh2LFXOs6GPCANCp68WRze+TlNJD3dUtkg3AB1LkJUsnqQ1XAAAAAElFTkSuQmCC'});
+/* payload */
+// red
+var icon = new LeafIcon({iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABmJLR0QA/wD/AP+gvaeTAAAAv0lEQVQYlTXPIU8CYQCA4ecDIZlMUBmb3epvOGYmOjVRbhb/g93oHyAddJKbg81sh00OdzvGeZDwzqC+9UkvvzVwNyC5JMENwp8JD0zXcVyvhsN63u/XT9Q9JghN3I7j+P5sNALt7ZaiEMryfM6qEREdNxv72cwxTakq7VZLFx0GJzn1x2KhU5ZUlf1yqTgc7FChueL0Is8HisJXlknz3DrLvOKNx4DQI4mIutjhHS9MMq7+VwKuu0Tf+GSKZ9Q/0IFOVMgrTdIAAAAASUVORK5CYII%3D'});
+// blue
+var iconLandkreis = new LeafIcon({iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABmJLR0QA/wD/AP+gvaeTAAAA3klEQVQYlT2PsU7CQABA3yGXQBhdbN2I0Z3Vb7hGJxNHo04uxIWV+AuGEb+h7ayTDrK4aIjBSZdKzqINd6Q13DFgfOvLGx6sqQHnhFHM5n4MnALizyHY7aXd64k/vnrxO0e3ns7A02ongNgAzrr9m8uLw21AMDOCwjrmldwjf/yoESr1Oau4e/omyyucByklNAJobkV1ytyPnjPmiyXOw3tmsNbC7w84R52vh/Rt8hoZY5BSYq1Faw3FGMppIgBBqx0TKkUjWJfFGPR9QqUPxP8JnNAMFG4J5TQFhoBfAVS8XfVK8EgaAAAAAElFTkSuQmCC'});
+// yellow
+var iconAusarbeitung = new LeafIcon({iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH2wcOEyUlW0X05gAAANNJREFUGJU1z7EOwVAAQNH7lE6dxNCuIrH3P9qdUWKziIHJD9iNfsDGbpI0lYjRYvISLZHSajtVnwF3PdPV+FYB+q7LtNGgIyUGcPgZYjJhHQRDJWVX+X5LzeeoZpMVIDSgv1wOR/X6AABdfwAJQqRt30dWHAenKK5k2YaiCIESXa9hWWCauNUoQl0uO0wzBUqy7EyS5MQxlCVoUmLYduRCwut1JwwjguCO58F+z0wDDscjthBp+/mMOZ1yPA+2W1Z5zlj8T4CeZeG833C7sQYWgPoAC4FeOn1WF+AAAAAASUVORK5CYII%3D'});
+// black
+var iconausserkraft = new LeafIcon({iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABmJLR0QA/wD/AP+gvaeTAAAAcklEQVQYlXWPSwqDQBBE35szuDQ70UPFEybiGfRAisZ9Jos0cQhYUFBNV/UHvripg/oKjkBL0VzU/McVqIlkBnp1UifgHqYn6hHFDFRAFcas7okT70J7Kh2LFXOs6GPCANCp68WRze+TlNJD3dUtkg3AB1LkJUsnqQ1XAAAAAElFTkSuQmCC'});
